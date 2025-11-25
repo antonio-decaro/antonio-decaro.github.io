bdce51827ff6dfcfb8756ad8e57a0617e9f32fba
antonio-decaro <antonio.decaro99@outlook.it>
2025-11-25 15:42:06 +0000

    Deploying to gh-pages from @ antonio-decaro/antonio-decaro.github.io@705792b44e03db6d5aacf94d9a0441eadf28d3fd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-teaching",
+          title: "Teaching",
+          description: "Materials for courses and thesis projects for students.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "nav-cv",
           title: "CV",
           description: "",
